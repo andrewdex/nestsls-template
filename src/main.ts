c284acc7a59cf2d3f22126821ac8c536d03fc3be
Dilusha Gonagala <andrewdexis@gmail.com>
2023-12-27 19:23:54 -0700

    use jsonpplaceholder

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // This will enable CORS for all routes
   app.setGlobalPrefix('/api'); // This will be your API's root URL
   await app.listen(3000);
 }
